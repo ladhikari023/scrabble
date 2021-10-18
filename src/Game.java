33feb43c2355302619788bc99f8099e6748a1baa
Laxman Adhikari <ladhikari023@unm.edu>
2021-10-18 13:49:43 -0600

    Game Committed

---
+++ src/Game.java
@@ -0,0 +1,23 @@
+import javafx.scene.layout.GridPane;
+
+public class Game {
+    public GridPane gridPane;
+
+    public Game(){}
+    public Game(GridPane gridPane){
+        this.gridPane = gridPane;
+    }
+
+    // ShowGrid
+        // Make Grid Pane
+        // Populate Grid Pane with the special boxes
+        // Add functionality to multiply the words or letters score in special boxes
+    // ShowWords
+        // Show the letters from the 100 letters
+        // Add exchange letters functionality
+        // Add words drag functionality
+    // CheckDrag
+        // check if the cell we are dragging our letter is valid when the user hits enter/play btn
+    // Add Computer Logic to play the best word possible
+    // ShowScore - update the score everytime user hits the enter/play btn
+}
